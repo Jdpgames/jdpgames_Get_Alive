77a802eb702eeb6aa7de88c8594d63998346fa02
Jdpgames <46383722+Jdpgames@users.noreply.github.com>
2021-04-12 10:57:41 +0200

    Initial commit

---
+++ Assets/Scripts/_Core/PersistentSingleton.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+namespace ioc.getAlive.Core
+{
+
+    public class PersistenSingleton<T> : MonoBehaviour where T : PersistenSingleton<T>
+    {
+        /// <summary> /// Singleton design pattern
+        /// </summary>
+        /// <value>The instance.</value>
+        public static T Instance
+        {
+            get { return ((T)SingletonInstance); }
+            set { SingletonInstance = value; }
+        }
+        private static T m_instance;
+        protected bool m_enabled;
+
+        protected static PersistenSingleton<T> SingletonInstance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    m_instance = FindObjectOfType<T>();
+                    if (m_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        m_instance = obj.AddComponent<T>();
+                    }
+                }
+                return m_instance;
+            }
+
+            set
+            {
+                m_instance = value as T;
+            }
+        }
+
+
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (m_instance == null)
+            {
+                //If I am the first instance, make me the Singleton
+                m_instance = this as T;
+                DontDestroyOnLoad(transform.gameObject);
+                m_enabled = true;
+            }
+            else
+            {
+                //If a Singleton already exists and you find
+                //another reference in scene, destroy it!
+                if (this != m_instance)
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/_Core/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace ioc.getAlive.Core
+{
+    /// <summary>
+    /// Singleton pattern.
+    /// </summary>
+    /// <typeparam name="T">Instantiation type of this singleton </typeparam>
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        protected static T _instance;
+
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+
+            }
+        }
+
+
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            _instance = this as T;
+        }
+    }
+
+}
