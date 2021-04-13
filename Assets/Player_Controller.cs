49f6661ab4f96cf3141658a424faae13029dc7f4
Jdpgames <46383722+Jdpgames@users.noreply.github.com>
2021-04-13 11:19:59 +0200

    Player_controller y AnimatorController

---
+++ Assets/Player_Controller.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ioc.getAlive.Controller
+{
+    public class Player_Controller : MonoBehaviour
+    {
+
+        public float RunSpeed = 3;
+        public float JumpSpeed = 5;
+        Rigidbody2D rb2D;
+        public SpriteRenderer mysprite;
+        public Animator anim;
+
+
+        void Start()
+        {
+
+            rb2D = GetComponent<Rigidbody2D>();
+
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (Input.GetKey("d"))
+            {
+               rb2D.velocity = new Vector2(RunSpeed, rb2D.velocity.y);
+                anim.SetBool("Run", true);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+
+            }
+
+            else if ( Input.GetKey("a"))
+            {
+
+                rb2D.velocity = new Vector2(-RunSpeed, rb2D.velocity.y);
+
+                anim.SetBool("Run", true);
+                transform.eulerAngles = new Vector3(0, 180, 0);
+
+
+            }
+
+
+            else
+            {
+                rb2D.velocity = new Vector2(0, rb2D.velocity.y);
+                anim.SetBool("Run", false);
+            }
+
+            if (Input.GetKey("space")&& checkground.isGrounded)
+            {
+                rb2D.velocity = new Vector2(rb2D.velocity.x, JumpSpeed);
+
+
+            }
+
+
+
+
+            if (checkground.isGrounded==false)
+            {
+                anim.SetBool("Jump", true);
+                anim.SetBool("Run", false);
+
+            }
+
+            else if (checkground.isGrounded==true)
+            {
+                anim.SetBool("Jump", false);
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
