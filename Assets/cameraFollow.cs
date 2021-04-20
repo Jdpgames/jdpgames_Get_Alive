3df347ae3f1e57feecd2ef0c097b08ce56415e06
Jdpgames <46383722+Jdpgames@users.noreply.github.com>
2021-04-20 23:05:32 +0200

    CameraFollow

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public GameObject john;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    { // posicion de john en x, altura 0 y -10 en profundidad. 
+        if (john != null)
+        {
+
+            transform.position = new Vector3(john.transform.position.x, 0, -10);
+
+        }
+    }
+}
