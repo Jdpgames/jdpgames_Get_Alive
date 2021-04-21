eb6cadb5330ce54dac2f1de37a687c2f1a532dbe
Jdpgames <46383722+Jdpgames@users.noreply.github.com>
2021-04-21 21:34:35 +0200

    Música
    Creación del script MusicManager que nos permite controlar el volumen de todas las escenas del juego.

---
+++ Assets/Scripts/_others/VolumeControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioSource AudioSource;
+    public Slider volumeControl;
+    private float MusicVolume = 0f;
+
+    private void Start()
+    {
+        AudioSource.Play();
+        MusicVolume = PlayerPrefs.GetFloat("Volume");
+        AudioSource.volume = MusicVolume;
+        volumeControl.value = MusicVolume;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioSource.volume = MusicVolume;
+        PlayerPrefs.SetFloat("Volume", MusicVolume);
+    }
+
+    public void VolumeUpdate(float volume)
+    {
+        MusicVolume = volume;
+
+
+    }
+}
